69548ce8511e36331047b9c781ae3e4ceb4da925
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-04 22:40:17 +0100

    Pulse Input Work

--- Run.py
+++ Run.py
@@ -7,11 +7,11 @@ from WeightInput import *
 def main() :
     pins = { 0 : 22 }
     motor_controller = LEDMotorController(pins)
-    wii_board = WiiBoard()
-    controller = Controller(motor_controller, wii_board)
+    weight_input = PulseInput(5)
+    controller = Controller(motor_controller, weight_input)
 
     while True :
-        if wii_board.get_weight() < 20 :
+        if weight_input.get_weight() < 100 :
             motor_controller.turn_on_motor(0)
         else :
             motor_controller.turn_off_motor(0)
