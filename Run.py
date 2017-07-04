053af98aecce9c72e1d8b826ef567a0cfe43440a
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-04 20:05:09 +0100

    Main Method behaviour created and WiiBoard.py added

--- Run.py
+++ Run.py
@@ -3,10 +3,14 @@ import time
 from Controller import *
 from MotorController import *
 
-pins = { 0 : 4 }
-motor_controller = LEDMotorController(pins)
-controller = Controller(motor_controller)
+def main() :
+    pins = { 0 : 4 }
+    motor_controller = LEDMotorController(pins)
+    controller = Controller(motor_controller)
 
-controller.motor_controller.turn_on_motor(0)
-time.sleep(1)
-controller.motor_controller.turn_off_motor(0)
+    controller.motor_controller.turn_on_motor(0)
+    time.sleep(1)
+    controller.motor_controller.turn_off_motor(0)
+
+if __name__ == "__main__":
+    main()
