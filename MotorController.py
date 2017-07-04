39f562fd2ea81314d1b35ad86ff03cc4c2d5c8d9
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-04 22:47:42 +0100

    Changed print statements

--- MotorController.py
+++ MotorController.py
@@ -6,7 +6,6 @@ class MotorController(ABC) :
 
     def __init__(self, pin_numbers) :
         self.pin_numbers = pin_numbers
-        GPIO.setmode(GPIO.BCM)
         for key in pin_numbers :
             GPIO.setup(pin_numbers[key], GPIO.OUT)
 
@@ -26,11 +25,11 @@ class MotorController(ABC) :
 class LEDMotorController(MotorController) :
 
     def turn_on_motor(self, motor_number) :
-        print("Turning on LED")
-        print(self.pin_numbers[motor_number])
+        #print("Turning on LED")
+        #print(self.pin_numbers[motor_number])
         GPIO.output(self.pin_numbers[motor_number], True)
 
     def turn_off_motor(self, motor_number) :
-        print("Turning off LED")
-        print(self.pin_numbers[motor_number])
+        #print("Turning off LED")
+        #print(self.pin_numbers[motor_number])
         GPIO.output(self.pin_numbers[motor_number], False)
--- Run.py
+++ Run.py
@@ -5,13 +5,16 @@ from MotorController import *
 from WeightInput import *
 
 def main() :
+    GPIO.setmode(GPIO.BCM)
     pins = { 0 : 22 }
     motor_controller = LEDMotorController(pins)
     weight_input = PulseInput(5)
     controller = Controller(motor_controller, weight_input)
 
     while True :
-        if weight_input.get_weight() < 100 :
+        weight = weight_input.get_weight()
+        print(weight)
+        if weight < 100 :
             motor_controller.turn_on_motor(0)
         else :
             motor_controller.turn_off_motor(0)
