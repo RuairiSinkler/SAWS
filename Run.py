82b5db90003f4f155d1e83407b31b28a836ccef5
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-04 21:08:03 +0100

    Trying to integrate WiiBoard weighing

--- Run.py
+++ Run.py
@@ -2,16 +2,19 @@
 import time
 from Controller import *
 from MotorController import *
+from WeightInput import *
 
 def main() :
     pins = { 0 : 22 }
     motor_controller = LEDMotorController(pins)
-    controller = Controller(motor_controller)
+    wii_board = WiiBoard()
+    controller = Controller(motor_controller, wii_board)
 
-    controller.motor_controller.turn_on_motor(0)
-    time.sleep(1)
-    controller.motor_controller.turn_off_motor(0)
-    GPIO.cleanup()
+    for i in range(100) :
+        if wii_board.get_weight() < 20 :
+            motor_controller.turn_on_motor(0)
+        else :
+            motor_controller.turn_off_motor(0)
 
 if __name__ == "__main__":
     main()
