23511cfbfe7b99967ceb3376d2c34038d2405ff9
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-05 14:19:04 +0100

    Testing more LEDs

--- Run.py
+++ Run.py
@@ -6,7 +6,7 @@ from WeightInput import *
 
 def main() :
     GPIO.setmode(GPIO.BCM)
-    pins = { 0 : 22 }
+    pins = { 0 : 16 , 1 : 13 , 2 : 19 , 3 : 26 }
     motor_controller = LEDMotorController(pins)
     weight_input = PulseInput(5)
     controller = Controller(motor_controller, weight_input)
@@ -15,9 +15,11 @@ def main() :
         weight = weight_input.get_weight()
         print(weight)
         if weight < 100 :
-            motor_controller.turn_on_motor(0)
+            for i in range(0, 3) :
+                motor_controller.turn_on_motor(i)
         else :
-            motor_controller.turn_off_motor(0)
+            for i in range(0, 3):
+                motor_controller.turn_off_motor(i)
         time.sleep(0.02)
 
 if __name__ == "__main__":
