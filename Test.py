d9dfa347bff5daa115361b953fd6d486c9652eb1
Ruairi Sinkler <sinky_son@hotmail.co.uk>
2017-07-12 19:07:32 +0100

    Added database management code to test

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ from DatabaseManagement import *
 
 def main() :
     manager = DatabaseManager("test.db")
-    manager.insert("test_table", {"test1", "test2", "test3"})
+    manager.insert("test_table", ["test1", "test2", "test3"])
 
 if __name__ == "__main__":
     main()
