791974db2dd1a76ddbdbd5eeddc3df7b64d0c8ed
Ruairi Sinkler <sinky_son@hotmail.co.uk>
2017-07-12 19:27:23 +0100

    Removed some methods to streamline to try and fix database

--- DatabaseManagement.py
+++ DatabaseManagement.py
@@ -5,10 +5,10 @@ class DatabaseManager :
 
     def __init__(self, database_name) :
         self.database_name = database_name
-        self.connect = sqlite3.connect(self.database_name)
 
     def insert(self, table, values) :
-        cursor = self.get_cursor()
+        connect = sqlite3.connect(self.database_name)
+        cursor = connect.cursor()
 
         values_string = "\"" + values[0] + "\""
         for i in range(1, len(values)) :
@@ -19,13 +19,7 @@ class DatabaseManager :
         print(execution)
         cursor.execute(execution)
 
-        self.close_connection()
-
-    def get_cursor(self) :
-        cursor = self.connect.cursor()
-        return cursor
-
-    def close_connection(self) :
-        self.connect.close()
+        connect.close()
+        
 
 
