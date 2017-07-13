fddbce8b47d53053be5eb24f7d95ca24e85db996
Ruairi Pi <risinkler1@sheffield.ac.uk>
2017-07-13 17:17:33 +0000

    Added Test ration to database for use in testing

--- Test.py
+++ Test.py
@@ -2,6 +2,8 @@
 from DatabaseManagement import *
 
 def main() :
+    print([1, 2, 3] == [1, 2, 3])
+    print([1, 2, 3] == [3, 2, 1])
     ration_manager = DatabaseManager("rations.db")
     print(ration_manager.get_ration("Minnie"))
 
