462650e71ec6a43b47726176fc945d2effb70f7b
Fuzznut <sinky_son@hotmail.co.uk>
2017-07-22 15:34:58 +0100

    fixed display option for assignments

--- Display.py
+++ Display.py
@@ -58,14 +58,14 @@ class ConsoleDisplay(Display) :
         self.display_weights(end_weights, weight_limits)
 
     def print_assignment(self, assignment) :
-        print(("{a[0]:^9} | {a[1]:^15}").format(a=assignment)
+        print(("{a[0]:^10} | {a[1]:^15}").format(a=assignment)
               )
-        print("{:-^27}".format(""))
+        print("{:-^28}".format(""))
 
     def assignments(self) :
         assignments = self.ration_database.get_assignments()
-        print(("{:^9} | {:^15}").format("House", "Ration"))
-        print("{:-^27}".format(""))
+        print(("{:^10} | {:^15}").format("House", "Ration"))
+        print("{:-^28}".format(""))
         for assignment in assignments :
             self.print_assignment(assignment)
 
