d5499ee4e604c2fbed6492d6a738d367d70d5eb4
Ruairi Sinkler <risinkler1@sheffield.ac.uk>
2018-05-23 01:23:57 +0100

    Further increased time between readings to decrease bounce

--- SAWS.py
+++ SAWS.py
@@ -584,7 +584,7 @@ class WeightInput():
             self.state = newstate
             if newstate == GPIO.LOW:
                 self.parent.increment_value(self.weigher)
-        self.controller.after(20, self.check_input)
+        self.controller.after(100, self.check_input)
 
 def main():
     # root = tk.Tk()
