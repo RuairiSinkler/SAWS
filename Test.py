c868c5cc6976dadfa4eef8722cf8e21891ee015c
Ruairi Sinkler <risinkler1@sheffield.ac.uk>
2018-05-25 16:57:43 +0100

    Button test added

--- Test.py
+++ Test.py
@@ -1,4 +1,36 @@
-import ExcelManagement as ex
+import RPI.GPIO as GPIO
+import tkinter as tk
 
-book = ex.WorksheetManager("./", "test")
-print(book.find("Time Run"))
+class WeightInput():
+
+    def __init__(self, weight_pin):
+        self.pin = weight_pin
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(self.pin, GPIO.RISING, callback=lambda: print("button pressed"))
+        # self.state = GPIO.input(self.pin)
+        # self.check_input()
+
+    def check_input(self):
+        oldstate = self.state
+        newstate = GPIO.input(self.pin)
+        if oldstate != newstate:
+            self.state = newstate
+            if newstate == GPIO.HIGH:
+                self.parent.increment_value(self.weigher)
+        self.controller.after(200, self.check_input)
+
+def main():
+    try:
+        WeightInput(19)
+        WeightInput(26)
+        input("Press enter to exit:")
+    except:
+        pass
+    finally:
+        GPIO.cleanup()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        raise
