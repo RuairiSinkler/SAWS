6a856377dc6db92e13110c34b14f3781fef1d623
Ruairi Sinkler <r.sinkler17@gmail.com>
2020-07-27 10:24:20 +0100

    re-added configure methods to canvas

--- SAWS.py
+++ SAWS.py
@@ -260,24 +260,24 @@ class MainMenu(tk.Frame):
         interior_id = canvas.create_window(0, 0, window=interior,
                                            anchor=tk.NW)
 
-        # # track changes to the canvas and frame width and sync them,
-        # # also updating the scrollbar
-        # def _configure_interior(event):
-        #     # update the scrollbars to match the size of the inner frame
-        #     size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-        #     canvas.config(scrollregion="0 0 %s %s" % size)
-        #     if interior.winfo_reqwidth() != canvas.winfo_width():
-        #         # update the canvas's width to fit the inner frame
-        #         canvas.config(width=interior.winfo_reqwidth())
-        #
-        # interior.bind('<Configure>', _configure_interior)
-        #
-        # def _configure_canvas(event):
-        #     if interior.winfo_reqwidth() != canvas.winfo_width():
-        #         # update the inner frame's width to fill the canvas
-        #         canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        #
-        # canvas.bind('<Configure>', _configure_canvas)
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+
+        canvas.bind('<Configure>', _configure_canvas)
 
         for ration in rations:
             id = ration[0]
@@ -289,7 +289,7 @@ class MainMenu(tk.Frame):
             button.pack(padx=10, pady=5, side=tk.TOP)
 
         button = tk.Button(
-            interior, text="Quit", font=controller.mainFont, command=lambda: controller.show_frame("SplashPage")
+            self, text="Quit", font=controller.mainFont, command=lambda: controller.show_frame("SplashPage")
         )
         button.pack(fill="x")
 
