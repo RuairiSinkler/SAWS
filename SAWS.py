12d9de28c09fb2773b23a6d6d1f83c64631413ac
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-04 10:36:01 +0100

    I honestly don't know what fixed it, but the scrolling ration page works now

--- SAWS.py
+++ SAWS.py
@@ -308,7 +308,6 @@ class RationPage(tk.Frame):
         self.ration_id = ration_id
 
         self.name = self.controller.ration_db.get_ration(self.ration_id)[1]
-        print(self.name)
         ingredients = self.controller.ration_db.get_ration_ingredients(ration_id)
         label = tk.Label(
             self.main, text=self.name, font=self.controller.mainFont
@@ -318,7 +317,6 @@ class RationPage(tk.Frame):
         ingredients_list = VerticalScrolledFrame(self.main)
         ingredients_list.pack(fill=tk.BOTH, expand=tk.TRUE)
         for ingredient in ingredients:
-            print("{}, {}kg".format(ingredient[0], str(ingredient[1])))
             label = tk.Label(
                 ingredients_list.interior, text="{}, {}kg".format(ingredient[0], str(ingredient[1])), font=self.controller.mainFont
             )
