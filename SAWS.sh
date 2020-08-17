ddc6e46dc3f86048125d26513dfe1e874aae01ef
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-17 11:17:52 +0100

    Further improved SAWS.sh

--- SAWS.sh
+++ SAWS.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-for i in {1..5};
-do 
-    sudo python3 SAWS.py && break || sleep 2;
+success=false
+for i in {1..5}; do
+    if sudo python3 SAWS.py; then
+        success=true
+        break
+    else
+        sleep 2
+    fi
 done
+
+if ! success: then
+    zenity --error --text="Tried to start SAWS.py 5 times\!" --title="Warning\!"
+fi
