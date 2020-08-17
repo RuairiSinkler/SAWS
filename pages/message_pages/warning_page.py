32d74427e5b576c50e7053c9385de4e1842d3d22
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-17 11:46:38 +0100

    Fixed Continue button on Warning Pages

--- pages/message_pages/warning_page.py
+++ pages/message_pages/warning_page.py
@@ -8,15 +8,15 @@ class WarningPage(tk.Frame):
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(3, weight=1)
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(3, weight=1)
-        
+        self.grid_columnconfigure(2, weight=1)
+
         self.name = name
         self.active = False
         self.temp = temp
 
         self.message = tk.StringVar()
         w = tk.Label(self, textvariable=self.message, font=self.controller.mainFont)
-        w.grid(row=1, column=1, columnspan=2)
+        w.grid(row=1, column=1)
         button = tk.Button(
             self, text="Continue", font=self.controller.mainFont, command=lambda:self.hide_page()
         )
