41034b18793d2a6136a89e9ad9214fff4444775f
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-25 17:24:10 +0100

    Basic movement of tables added, still flawed at the current time

--- Test.py
+++ Test.py
@@ -7,221 +7,137 @@ from tkinter import ttk
 
 import exceptions as err
 
-class WarningPage(tk.Frame):
+import excel.excel_management as ex
 
-    def __init__(self, parent, controller, name="WarningPage", temp=False):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(3, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(2, weight=1)
+# class WarningPage(tk.Frame):
 
-        self.name = name
-        self.active = False
-        self.temp = temp
+#     def __init__(self, parent, controller, name="WarningPage", temp=False):
+#         tk.Frame.__init__(self, parent)
+#         self.controller = controller
+#         self.grid_rowconfigure(0, weight=1)
+#         self.grid_rowconfigure(3, weight=1)
+#         self.grid_columnconfigure(0, weight=1)
+#         self.grid_columnconfigure(2, weight=1)
 
-        self.message = tk.StringVar()
-        w = tk.Label(self, textvariable=self.message, font=self.controller.mainFont)
-        w.grid(row=1, column=1)
-        button = tk.Button(
-            self, text="Continue", font=self.controller.mainFont, command=lambda:w.textvariable.set("Poop")
-        )
-        button.grid(row=2, column=1, sticky="ew")
+#         self.name = name
+#         self.active = False
+#         self.temp = temp
 
-    def display_page(self, warning, belowThis=None):
-        self.active = True
-        self.message.set(warning.message)
-        self.controller.show_frame(self.name, belowThis=belowThis)
+#         self.message = tk.StringVar()
+#         w = tk.Label(self, textvariable=self.message, font=self.controller.mainFont)
+#         w.grid(row=1, column=1)
+#         button = tk.Button(
+#             self, text="Continue", font=self.controller.mainFont, command=lambda:w.textvariable.set("Poop")
+#         )
+#         button.grid(row=2, column=1, sticky="ew")
+
+#     def display_page(self, warning, belowThis=None):
+#         self.active = True
+#         self.message.set(warning.message)
+#         self.controller.show_frame(self.name, belowThis=belowThis)
     
-    def hide_page(self):
-        self.active = False
-        self.controller.hide_frame(self.name)
-        if self.temp:
-            del self.controller.frames[self.name]
-            self.controller.warning_frames.remove(self)
-            self.destroy()
+#     def hide_page(self):
+#         self.active = False
+#         self.controller.hide_frame(self.name)
+#         if self.temp:
+#             del self.controller.frames[self.name]
+#             self.controller.warning_frames.remove(self)
+#             self.destroy()
 
-class Controller(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
+# class Controller(tk.Tk):
+#     def __init__(self, *args, **kwargs):
+#         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.screen_width = self.winfo_screenwidth() // 2
-        self.screen_height = self.winfo_screenheight() // 2
-        self.geometry("{0}x{1}+0+0".format(self.screen_width, self.screen_height))
+#         self.screen_width = self.winfo_screenwidth() // 2
+#         self.screen_height = self.winfo_screenheight() // 2
+#         self.geometry("{0}x{1}+0+0".format(self.screen_width, self.screen_height))
 
-        self.container = tk.Frame(self)
-        self.container.pack(side="top", fill="both", expand=True)
-        self.container.grid_rowconfigure(0, weight=1)
-        self.container.grid_rowconfigure(2, weight=1)
-        self.container.grid_columnconfigure(0, weight=1)
-        self.container.grid_columnconfigure(2, weight=1)
+#         self.container = tk.Frame(self)
+#         self.container.pack(side="top", fill="both", expand=True)
+#         self.container.grid_rowconfigure(0, weight=1)
+#         self.container.grid_rowconfigure(2, weight=1)
+#         self.container.grid_columnconfigure(0, weight=1)
+#         self.container.grid_columnconfigure(2, weight=1)
 
-        self.mainFont = Font(size=25)
-        self.textFont = Font(size=15)
-        self.option_add('*Dialog.msg.font', self.mainFont)
-        self.option_add("*TCombobox*Listbox*Font", self.mainFont)
+#         self.mainFont = Font(size=25)
+#         self.textFont = Font(size=15)
+#         self.option_add('*Dialog.msg.font', self.mainFont)
+#         self.option_add("*TCombobox*Listbox*Font", self.mainFont)
 
-        self.frames = {}
-        self.warning_frames = []
+#         self.frames = {}
+#         self.warning_frames = []
 
-        page_name = "Base"
-        frame = tk.Frame(self.container)
-        self.frames[page_name] = frame
+#         page_name = "Base"
+#         frame = tk.Frame(self.container)
+#         self.frames[page_name] = frame
 
-        # put all of the pages in the same location;
-        # the one on the top of the stacking order
-        # will be the one that is visible.
-        #frame.grid(row=1, column=1, sticky="nsew")
-        frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
-        label = tk.Label(frame, text="Base Level")
-        label.pack()
+#         # put all of the pages in the same location;
+#         # the one on the top of the stacking order
+#         # will be the one that is visible.
+#         #frame.grid(row=1, column=1, sticky="nsew")
+#         frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
+#         label = tk.Label(frame, text="Base Level")
+#         label.pack()
 
-        warning_frame = self.create_frame(WarningPage, self.container)
-        self.warning_frames.append(warning_frame)
+#         warning_frame = self.create_frame(WarningPage, self.container)
+#         self.warning_frames.append(warning_frame)
         
 
 
-    def show_frame(self, page_name, aboveThis=None, belowThis=None):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        if belowThis:
-            frame.lower(belowThis)
-        else:
-            frame.lift(aboveThis)
+#     def show_frame(self, page_name, aboveThis=None, belowThis=None):
+#         '''Show a frame for the given page name'''
+#         frame = self.frames[page_name]
+#         if belowThis:
+#             frame.lower(belowThis)
+#         else:
+#             frame.lift(aboveThis)
             
 
-    def hide_frame(self, page_name):
-        '''Hide a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.lower()
+#     def hide_frame(self, page_name):
+#         '''Hide a frame for the given page name'''
+#         frame = self.frames[page_name]
+#         frame.lower()
 
-    def display_warning(self, warning):
-        if self.frames["WarningPage"].active:
-            current_timestamp = time.time_ns()
-            page_name = "TempWarningPage.{}".format(current_timestamp)
+#     def display_warning(self, warning):
+#         if self.frames["WarningPage"].active:
+#             current_timestamp = time.time_ns()
+#             page_name = "TempWarningPage.{}".format(current_timestamp)
 
-            frame = WarningPage(parent=self.container, controller=self, name=page_name, temp=True)
-            self.frames[page_name] = frame
+#             frame = WarningPage(parent=self.container, controller=self, name=page_name, temp=True)
+#             self.frames[page_name] = frame
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
-            #frame.grid(row=1, column=1, sticky="nsew")
-            frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
+#             # put all of the pages in the same location;
+#             # the one on the top of the stacking order
+#             # will be the one that is visible.
+#             #frame.grid(row=1, column=1, sticky="nsew")
+#             frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
 
-            self.frames[page_name].display_page(warning, belowThis=self.warning_frames[-1])
-            self.warning_frames.append(frame)
-        else:
-            self.frames["WarningPage"].display_page(warning)
+#             self.frames[page_name].display_page(warning, belowThis=self.warning_frames[-1])
+#             self.warning_frames.append(frame)
+#         else:
+#             self.frames["WarningPage"].display_page(warning)
             
 
-    def create_frame(self, F, container, name=None, *args):
-        page_name = F.__name__
-        if name is not None:
-            page_name = name
-        frame = F(parent=container, controller=self, *args)
-        self.frames[page_name] = frame
+#     def create_frame(self, F, container, name=None, *args):
+#         page_name = F.__name__
+#         if name is not None:
+#             page_name = name
+#         frame = F(parent=container, controller=self, *args)
+#         self.frames[page_name] = frame
 
-        # put all of the pages in the same location;
-        # the one on the top of the stacking order
-        # will be the one that is visible.
-        #frame.grid(row=1, column=1, sticky="nsew")
-        frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
-        return frame
+#         # put all of the pages in the same location;
+#         # the one on the top of the stacking order
+#         # will be the one that is visible.
+#         #frame.grid(row=1, column=1, sticky="nsew")
+#         frame.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor=tk.CENTER)
+#         return frame
         
-root = Controller()
-root.display_warning(err.SAWSWarning("Warning 1"))
-root.display_warning(err.SAWSWarning("Warning 2"))
-root.display_warning(err.SAWSWarning("Warning 3"))
-root.display_warning(err.SAWSWarning("Warning 4"))
-root.mainloop()
+# root = Controller()
+# root.display_warning(err.SAWSWarning("Warning 1"))
+# root.display_warning(err.SAWSWarning("Warning 2"))
+# root.display_warning(err.SAWSWarning("Warning 3"))
+# root.display_warning(err.SAWSWarning("Warning 4"))
+# root.mainloop()s
 
-
-
-
-
-# class VerticalScrolledFrame(tk.Frame):
-#     """A pure Tkinter scrollable frame that actually works!
-
-#     * Use the 'interior' attribute to place widgets inside the scrollable frame
-#     * Construct and pack/place/grid normally
-#     * This frame only allows vertical scrolling
-#     """
-#     def __init__(self, parent, *args, **kw):
-#         tk.Frame.__init__(self, parent, *args, **kw)
-
-#         # create a canvas object and a vertical scrollbar for scrolling it
-#         vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
-#         vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-#         canvas = tk.Canvas(self, bd=0, highlightthickness=0,
-#                         yscrollcommand=vscrollbar.set)
-#         canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-#         vscrollbar.config(command=canvas.yview)
-        
-#         # reset the view
-#         canvas.xview_moveto(0)
-#         canvas.yview_moveto(0)
-        
-#         # create a frame inside the canvas which will be scrolled with it
-#         self.interior = interior = tk.Frame(canvas)
-#         interior_id = canvas.create_window(0, 0, window=self.interior,
-#                                            anchor=tk.NW)
-        
-#         # track changes to the canvas and frame width and sync them,
-#         # also updating the scrollbar
-#         def _configure_interior(event):
-#             # update the scrollbars to match the size of the inner frame
-#             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-#             canvas.config(scrollregion="0 0 %s %s" % size)
-#             if interior.winfo_reqwidth() != canvas.winfo_width():
-#                 # update the canvas's width to fit the inner frame
-#                 canvas.config(width=interior.winfo_reqwidth())
-        
-#         interior.bind('<Configure>', _configure_interior)
-        
-#         def _configure_canvas(event):
-#             if interior.winfo_reqwidth() != canvas.winfo_width():
-#                 # update the inner frame's width to fill the canvas
-#                 canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-#         canvas.bind('<Configure>', _configure_canvas)
-
-
-# ingredients = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-
-# main = tk.Frame(root)
-# main.pack()
-# footer = tk.Frame(root)
-# footer.pack(side=tk.BOTTOM)
-
-# button = tk.Button(
-#     footer, text="Back"
-# )
-
-# button.pack(side=tk.LEFT)
-
-# button = tk.Button(
-#     footer, text="Run"
-# )
-
-# button.pack(side=tk.LEFT)
-
-# main.destroy()
-# main = tk.Frame(root)
-# main.pack(fill=tk.BOTH, expand=tk.TRUE)
-
-# name = "Ration Name"
-# label = tk.Label(
-#     main, text=name
-# )
-# label.pack()
-
-# ingredients_list = VerticalScrolledFrame(main)
-# ingredients_list.pack(fill=tk.BOTH, expand=tk.TRUE)
-# for ingredient in ingredients:
-#     label = tk.Label(
-#         ingredients_list.interior, text="{}kg".format(ingredient)
-#     )
-#     label.pack()
-
-# root.mainloop()
+excel = ex.WorksheetManager("./", "rations")
+excel.update_sheets("rations")
--- excel/excel_management.py
+++ excel/excel_management.py
@@ -1,7 +1,11 @@
 import os
 import openpyxl
+import itertools
+import numpy as np
 from pathlib import Path
 
+RATIONS_SHEET_VERSION = 2.0
+RATION_LOGS_SHEET_VERSION = 1.0
 
 class WorksheetManager:
     def __init__(self, directory, name):
@@ -23,27 +27,6 @@ class WorksheetManager:
     def create_sheet(self, title):
         self.workbook.create_sheet(title)
         self.save()
-
-    def log_run(self, time_run, ration, complete, ingredients, batch_number):
-        row = self.sheet.max_row + 1
-        column = self.find("Date Run").column
-        self.write_cell(time_run, self.get_cell(column, row))
-        self.write_cell(ration, self.get_cell(column + 1, row))
-        if complete:
-            complete = "Yes"
-        else:
-            complete = "No"
-        self.write_cell(complete, self.get_cell(column + 2, row))
-        total = 0
-        for ingredient in ingredients:
-            column = self.find(ingredient.name).column
-            self.write_cell(ingredient.current_amount, self.get_cell(column, row))
-            total += ingredient.current_amount
-        column = self.find("Total").column
-        self.write_cell(total, self.get_cell(column, row))
-        column = self.find("Batch Number").column
-        self.write_cell(batch_number, self.get_cell(column, row))
-
 
     # Returns a list of the sheets in the workbook
     def get_sheets(self):
@@ -90,3 +73,162 @@ class WorksheetManager:
                 if self.read_cell(cell) == search_string:
                     return cell
         return None
+
+    def log_run(self, time_run, ration, complete, ingredients, batch_number):
+        row = self.sheet.max_row + 1
+        column = self.find("Date Run").column
+        self.write_cell(time_run, self.get_cell(column, row))
+        self.write_cell(ration, self.get_cell(column + 1, row))
+        if complete:
+            complete = "Yes"
+        else:
+            complete = "No"
+        self.write_cell(complete, self.get_cell(column + 2, row))
+        total = 0
+        for ingredient in ingredients:
+            column = self.find(ingredient.name).column
+            self.write_cell(ingredient.current_amount, self.get_cell(column, row))
+            total += ingredient.current_amount
+        column = self.find("Total").column
+        self.write_cell(total, self.get_cell(column, row))
+        column = self.find("Batch Number").column
+        self.write_cell(batch_number, self.get_cell(column, row))
+
+    def update_sheets(self, sheet_type):
+        for sheet_name in self.get_sheets():
+            sheet = self.get_sheet(sheet_name)
+            self.change_sheet(sheet)
+
+            sheet_version = 0
+            version_cell = self.find("VERSION")
+            if version_cell is not None:
+                sheet_version = float(self.read_cell(self.get_cell(version_cell.column + 1, version_cell.row)))
+            
+            if sheet_type == "rations" and sheet_version < RATIONS_SHEET_VERSION:
+                if sheet_version < 1.0:
+                    sheet_version = 1.0
+                    version_cell = self._create_version_cell(sheet_version)
+                    self.save()
+                if sheet_version < 2.0:
+                    self._move_table(self.find("Ingredient"), 192, 3000)
+                    # sheet_version = 2.0
+                    # self._update_version_cell(sheet_version, version_cell)
+                    self.save()
+                    
+
+            elif sheet_type == "ration_logs" and sheet_version < RATION_LOGS_SHEET_VERSION:
+                pass
+    
+    def _create_version_cell(self, version):
+        version_cell = None
+        for row in self.sheet.rows:
+            for cell in row:
+                cell_value = self.read_cell(cell)
+                if cell_value is None and cell_value == '':
+                    if version_cell is not None:
+                        self.write_cell("VERSION", version_cell)
+                        self.write_cell(str(version), cell)
+                        return version_cell
+                    else:
+                        version_cell = cell
+                else:
+                    version_cell = None
+
+    def _update_version_cell(self, version, version_cell):
+        self.write_cell(version, self.get_cell(version_cell.column + 1, version_cell.row))
+
+    def _move_table(self, origin_cell, column_shift=0, row_shift=0):
+        origin_cell = self._get_upper_left_most_cell(origin_cell)
+        table_cells, column_bounds, row_bounds = self._get_table_cells(origin_cell)
+        column_order = 1
+        row_order = 1
+        if column_shift > 0:
+            column_order = -1
+            for row in range(row_bounds[0] - 1, row_bounds[1] + 2):
+                for shift in range(1, column_shift + 1):
+                    cell = self.get_cell(column_bounds[1] + shift, row)
+                    cell_value = self.read_cell(cell)
+                    if cell_value is not None and cell_value != '':
+                        self._move_table(cell, column_shift, row_shift) 
+        elif column_shift < 0:
+            for row in range(row_bounds[0] - 1, row_bounds[1] + 2):
+                for shift in range(1, abs(column_shift) + 1):
+                    cell = self.get_cell(origin_cell.column - shift, row)
+                    cell_value = self.read_cell(cell)
+                    if cell_value is not None and cell_value != '':
+                        self._move_table(cell, column_shift, row_shift)
+        if row_shift > 0:
+            row_order = -1
+            for column in range(column_bounds[0] - 1, column_bounds[1] + 2):
+                for shift in range(1, row_shift + 1):
+                    cell = self.get_cell(column, row_bounds[1] + shift)
+                    cell_value = self.read_cell(cell)
+                    if cell_value is not None and cell_value != '':
+                        self._move_table(cell, column_shift, row_shift)
+        elif row_shift < 0:
+            for column in range(column_bounds[0] - 1, column_bounds[1] + 2):
+                for shift in range(1, abs(row_shift) + 1):
+                    cell = self.get_cell(column, row_bounds[1] - shift)
+                    cell_value = self.read_cell(cell)
+                    if cell_value is not None and cell_value != '':
+                        self._move_table(cell, column_shift, row_shift)
+
+        self.sheet.move_range("{}:{}".format(table_cells[0, 0].coordinate, table_cells[-1, -1].coordinate), rows=row_shift, cols=column_shift)
+
+        # table_cells_view = table_cells[::row_order,::column_order]
+        # for i, row in enumerate(table_cells_view):
+        #     for j, cell in enumerate(row):
+        #         new_cell = self.get_cell(cell.column + column_shift, cell.row + row_shift)
+        #         self.write_cell(cell.value, new_cell)
+        #         self.write_cell("", cell)
+        #         cell = new_cell
+        #         table_cells_view[i, j] = cell
+        self.save()
+
+    def _get_upper_left_most_cell(self, cell):
+        if cell.row > 1:
+            cell_check = self.get_cell(cell.column, cell.row - 1)
+            cell_value = self.read_cell(cell_check)
+            while cell_value is not None and cell_value != '':
+                cell = cell_check
+                cell_check = self.get_cell(cell.column, cell.row - 1)
+                cell_value = self.read_cell(cell_check)
+        if cell.column > 1:
+            cell_check = self.get_cell(cell.column - 1, cell.row)
+            cell_value = self.read_cell(cell_check)
+            if cell_value is not None:
+                while cell_value is not None and cell_value != '':
+                    cell = cell_check
+                    cell_check = self.get_cell(cell.column - 1, cell.row)
+                    cell_value = self.read_cell(cell_check)
+                cell = self._get_upper_left_most_cell(cell)
+        return cell
+
+    def _get_table_cells(self, origin_cell):
+        origin_column = origin_cell.column
+        origin_row = origin_cell.row
+        column_bounds = [origin_column, origin_column]
+        row_bounds = [origin_row, origin_row]
+
+        row = row_bounds[0]
+        cell_value = self.read_cell(self.get_cell(origin_column, row))
+        while cell_value is not None and cell_value != '':
+            row_bounds[1] = max(row, row_bounds[1])
+            column = column_bounds[0]
+            cell_value = self.read_cell(self.get_cell(column, row))
+            while cell_value is not None and cell_value != '':
+                column_bounds[1] = max(column, column_bounds[1])
+                column += 1
+                cell_value = self.read_cell(self.get_cell(column, row))
+            row += 1
+            cell_value = self.read_cell(self.get_cell(origin_column, row))
+
+        cells = []
+        for row in range(row_bounds[0], row_bounds[1] + 1):
+            row_cells = []
+            for column in range(column_bounds[0], column_bounds[1] + 1):
+                row_cells.append(self.get_cell(column, row))
+            cells.append(row_cells)
+        return np.array(cells), column_bounds, row_bounds
+
+
