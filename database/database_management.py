b2790592c843058581c1cb9c81c9b44c1cbfe9f2
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-28 15:31:49 +0100

    printout

--- database/database_management.py
+++ database/database_management.py
@@ -114,6 +114,7 @@ class DatabaseManager:
         return result
 
     def get_weigher(self, weigher_id):
+        print("Getting weigher_id {}".format(weigher_id))
         connect = sqlite3.connect(self.database_name)
         cursor = connect.cursor()
 
