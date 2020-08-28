778df4d9f8f375f773c3ebe5f38f1d8bc89b3370
Fuzznut <sinky_son@hotmail.co.uk>
2020-08-28 11:03:01 +0100

    Fixed SUCCESS in bash script

--- SAWS.sh
+++ SAWS.sh
@@ -4,11 +4,11 @@ SUCCESS=false
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 for i in {1..5}; do
-    if ! $success; then
+    if ! $SUCCESS; then
         sudo python3 SAWS.py &
         sleep 1
         if pgrep "sudo python3 SAWS.py"; then
-            $success=true
+            $SUCCESS=true
             break
         else
             sleep 2
@@ -16,6 +16,6 @@ for i in {1..5}; do
     fi
 done
 
-if ! $success; then
+if ! $SUCCESS; then
     zenity --error --text="Tried to start SAWS.py 5 times\!" --title="Warning\!"
 fi
