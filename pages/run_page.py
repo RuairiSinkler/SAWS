8947ae80a4320aaeb4dd0a9232a869ea0615ae63
Fuzznut <sinky_son@hotmail.co.uk>
2020-09-24 14:14:32 +0100

    Turn of grid propagate first

--- pages/run_page.py
+++ pages/run_page.py
@@ -190,8 +190,8 @@ class RunPage(tk.Frame):
         self.main.update_idletasks()
         weigher_frame_width = self.controller.screen_width // len(self.weighers)
         for _, weigher in self.weighers.items():
-            weigher.frame.config(width=weigher_frame_width)
             weigher.frame.grid_propagate(False)
+            weigher.frame.config(width=weigher_frame_width)
             weigher.add_hopper()
 
         for ingredient, button in unweighed_ingredients:
