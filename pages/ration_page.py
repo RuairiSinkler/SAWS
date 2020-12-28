86f1384fb6047ae6400e356635a025d6b87c17ea
Fuzznut <sinky_son@hotmail.co.uk>
2020-12-28 14:23:21 +0000

    Dropdown on rationpage test

--- pages/ration_page.py
+++ pages/ration_page.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from pages.page_tools.vertical_scrolled_frame import VerticalScrolledFrame
 
 class RationPage(tk.Frame):
@@ -15,6 +16,8 @@ class RationPage(tk.Frame):
         self.ration_id = None
         self.name = None
 
+        self.house_dropdown = None
+
         button = tk.Button(
             self.footer, text="Back", font=self.controller.main_font,
             command=lambda: self.controller.show_frame("MainMenuPage")
@@ -23,7 +26,7 @@ class RationPage(tk.Frame):
         button.pack(side=tk.LEFT)
 
         button = tk.Button(
-            self.footer, text="Run", font=self.controller.main_font,
+            self.footer, text="Next", font=self.controller.main_font,
             command=lambda: self.controller.frames["RunPage"].display_page(self.ration_id)
         )
 
@@ -43,6 +46,12 @@ class RationPage(tk.Frame):
             self.main, text=self.name, font=self.controller.main_font
         )
         label.pack()
+
+        houses = self.controller.ration_db.get_all_houses()
+        house_names = [house[1] for house in houses]
+        self.house_dropdown = ttk.Combobox(self.main, values=house_names, state="readonly", font=self.controller.main_font)
+        self.house_dropdown.current(0)
+        self.house_dropdown.pack()
 
         ingredients_list = VerticalScrolledFrame(self.main)
         ingredients_list.pack(fill=tk.BOTH, expand=tk.TRUE)
