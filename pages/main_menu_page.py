6dbc11902825fec60f565576c1164aa0fbb4011a
Fuzznut <sinky_son@hotmail.co.uk>
2020-12-29 15:18:16 +0000

    Trying again to movee button to top

--- pages/main_menu_page.py
+++ pages/main_menu_page.py
@@ -12,17 +12,20 @@ class MainMenuPage(tk.Frame):
         title_label = tk.Label(self, text="Main Menu", font=self.controller.main_font)
         title_label.pack()
 
-        self.ration_options = VerticalScrolledFrame(self)
-        self.ration_options.pack(fill=tk.BOTH, expand=tk.TRUE)
+        ration_options = VerticalScrolledFrame(self)
+        ration_options.pack(fill=tk.BOTH, expand=tk.TRUE)
+
+        self.extra_rations_space = tk.Frame(ration_options.interior)
+        self.extra_rations_space.pack(side=tk.TOP)
 
         for ration in rations:
             id = ration[0]
             name = ration[1]
             button = tk.Button(
-                self.ration_options.interior, text=name, font=self.controller.main_font,
+                ration_options.interior, text=name, font=self.controller.main_font,
                 command=lambda id=id: self.controller.frames["RationPage"].display_page(id)
             )
-            button.pack(padx=10, pady=5)
+            button.pack(padx=10, pady=5, side=tk.TOP)
 
         button = tk.Button(
             self, text="Quit", font=self.controller.main_font, command=lambda: self.controller.show_frame("SplashPage")
@@ -34,7 +37,7 @@ class MainMenuPage(tk.Frame):
         id = ration[0]
         name = ration[1]
         button = tk.Button(
-            self.ration_options.interior, text=name, font=self.controller.main_font,
+            self.extra_rations_space, text=name, font=self.controller.main_font,
             command=lambda id=id: self.controller.frames["RationPage"].display_page(id)
         )
         button.pack(padx=10, pady=5, side=tk.TOP)
