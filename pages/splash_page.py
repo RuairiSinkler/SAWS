5d1b250d3affe3075a77b41b510ba3b30715a73f
Fuzznut <sinky_son@hotmail.co.uk>
2021-01-07 11:20:18 +0000

    Uniform on splash page

--- pages/splash_page.py
+++ pages/splash_page.py
@@ -5,12 +5,12 @@ class SplashPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(0, weight=1, uniform="splash_rows")
+        self.grid_rowconfigure(1, weight=1, uniform="splash_rows")
+        self.grid_rowconfigure(2, weight=1, uniform="splash_rows")
+        self.grid_columnconfigure(0, weight=1, uniform="splash_columns")
+        self.grid_columnconfigure(1, weight=1, uniform="splash_columns")
+        self.grid_columnconfigure(2, weight=1, uniform="splash_columns")
 
         button = tk.Button(
             self, text="Start", font=controller.main_font, command=lambda: self.controller.show_frame("PinPage")
